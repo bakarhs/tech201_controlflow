cb6f574d15a7f2b6546d03758c8ceb874e494255
Bakar <bakarhs123@gmail.com>
2023-01-31 15:26:13 +0000

    task 1 completed

---
+++ Task_2_control_flow/gross_pay_calculator.py
@@ -0,0 +1,11 @@
+hours_worked = float(input("How many hours have you worked this week?"))
+rate_of_pay = float(input("What is your rate of pay?"))
+
+if hours_worked < 40:
+    gross_pay = hours_worked * rate_of_pay
+else:
+    overtime = hours_worked - 40
+    overtime_pay = overtime * (rate_of_pay * 1.5)
+    gross_pay = (40 * rate_of_pay) + overtime_pay
+
+print(gross_pay)
